a40b0563be3ab474bf2f9475eb9ab1ae9ba53113
builtbysuraj <surajgupta3553@gmail.com>
2025-10-30 17:14:43 +0530

    chore: Add email status and inv status in conf and fix warnings in the header svg

--- src/conf/index.ts
+++ src/conf/index.ts
@@ -16,3 +16,20 @@ export const PERMISSIONS = {
     actions: ['view'],
   },
 } as const
+
+export const EMAIL_STATUS = {
+  NOT_ELIGIBLE: 'notEligible',
+  OVERDUE: 'overdue',
+  INSUFF_AVAIL_LIMIT: 'insufficientAvailableLimit',
+  ELIGIBLE: 'eligible',
+  SENT: 'sent',
+}
+
+export const INVOICE_STATUS = {
+  YET_TO_PROCESS: 'yetToProcess',
+  IN_PROGRESS: 'inProgress',
+  PROCESSED: 'processed',
+  PENDING_WITH_CUSTOMER: 'pendingWithCustomer',
+  PENDING_WITH_LENDER: 'pendingWithLender',
+  NOT_PROCESSED: 'notProcessed',
+}
