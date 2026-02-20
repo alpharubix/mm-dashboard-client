9006d767867c1b85ded073589b6efb06c1809e53
gitmyisa-beiucy <134397285+myisa-4112@users.noreply.github.com>
2026-02-20 12:59:12 +0530

    Comment out HolidayBanner component
    Comment out the HolidayBanner component in Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -23,7 +23,7 @@ export default function Home() {
   return (
     <main className='min-h-screen bg-gray-50'>
       <Header />
-      <HolidayBanner />
+      {/* <HolidayBanner /> */}
 
       <div className='container mx-auto px-4 sm:px-6 lg:px-8'>
         <div className=''>
