941bc105fe6cdd37745bf57104a8704b3679250d
myisa-4112 <myisabeiucy2002@gmail.com>
2026-03-18 19:20:07 +0530

    holiday banner for ugadi

--- src/components/HolidayBanner.tsx
+++ src/components/HolidayBanner.tsx
@@ -6,15 +6,16 @@ export default function HolidayBanner() {
       <div className='container mx-auto flex items-start gap-3 text-sm text-yellow-800 px-4 sm:px-6 lg:px-8'>
         <Bell className='h-5 w-5 text-yellow-600 mt-0.5 shrink-0' />
         <div className='space-y-2'>
-          <p>Disbursement Closed – <span className='font-semibold'>02nd & 03rd March 2026.</span></p>
           <p>
-            All lenders will remain closed on  02-03-2026 due to system migration
-            and on 03-03-2026 on account of Holi.
+            Kindle note that On{' '}
+            <span className='font-semibold'>19th March 2026</span> is a Holiday
+            "Ugadi"
           </p>
           <p>
-            Invoices received after the cut-off on 28-02-2026 will be processed
-            on 04-03-2026. The updated Limit Report will be shared on
-            04-03-2026
+            Transactions and invoices received after the cut-off time will be
+            processed on the next working day{' '}
+            <span className='font-semibold'>20th March 2026</span>. Kindly plan
+            your transactions accordingly.
           </p>
         </div>
       </div>
