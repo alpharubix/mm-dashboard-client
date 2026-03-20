d1b40a482b352969c02db887a7848dcf9e1434fb
myisa-4112 <myisabeiucy2002@gmail.com>
2026-03-20 17:06:20 +0530

    addded eid mubarak holiday banner

--- src/components/HolidayBanner.tsx
+++ src/components/HolidayBanner.tsx
@@ -7,15 +7,17 @@ export default function HolidayBanner() {
         <Bell className='h-5 w-5 text-yellow-600 mt-0.5 shrink-0' />
         <div className='space-y-2'>
           <p>
-            Kindle note that On{' '}
-            <span className='font-semibold'>19th March 2026</span> is a Holiday
-            "Ugadi"
+            Disbursement Closed –{' '}
+            <span className='font-semibold'> 21st March 2026</span>
           </p>
           <p>
-            Transactions and invoices received after the cut-off time will be
-            processed on the next working day{' '}
-            <span className='font-semibold'>20th March 2026</span>. Kindly plan
-            your transactions accordingly.
+            All lenders will remain closed on 21-03-2026 on account of
+            Eid-ul-Fitr (Ramzan-Id)
+          </p>
+          <p>
+            Invoices received after the cut-off on 20-03-2026 will be processed
+            on next working day (23-03-2026). The updated Limit Report will be
+            shared on 23-03-2026.
           </p>
         </div>
       </div>
